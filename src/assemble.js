cd26c0d541672fbaaa7205e5aec817890a007702
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-04-15 19:13:32 -0700

    Update assemble.js

--- src/assemble.js
+++ src/assemble.js
@@ -2,5 +2,16 @@ rom = [
 ]
 
 function assemble(){
-  
+
+  ops = ["ADD","BGE","NOR","RSH","LOD","STR","IN","OUT","PRI","IMM","JMP"]
+  reg = ["R0","R1","R2","R3","R4","R5","SP","PC"]
+
+
+  for(int i = 0; i < rom.length; i++){
+
+    m = rom[i].split(" ")
+    
+    
+  }
+
 }
