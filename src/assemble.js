c431bf7472bb891221b9b97231d07a061356b3d8
fenl <145521130+TenderGalaxy@users.noreply.github.com>
2025-04-17 16:39:38 -0700

    Update assemble.js

--- src/assemble.js
+++ src/assemble.js
@@ -1,17 +1,55 @@
 rom = [
+  "IMM 1 0"
 ]
 
-function assemble(){
 
-  ops = ["ADD","BGE","NOR","RSH","LOD","STR","IN","OUT","PRI","IMM","JMP"]
-  reg = ["R0","R1","R2","R3","R4","R5","SP","PC"]
-
-
-  for(int i = 0; i < rom.length; i++){
-
-    m = rom[i].split(" ")
-    
-    
+function functions(){
+  funs = {
+    "ADD" : "0000",
+    "BGE" : "0001",
+    "NOR" : "0002",
+    "RSH" : "0003",
+    "MOV" : "0004",
+    "IN"  : "0005",
+    "OUT" : "0006",
+    "PRI" : "0007",
+    "IMM" : "0008",
+    "JMP" : "0009",
+    "END" : "000A",
+    "SUB" : "000B",
+    "NOP" : "000C",
+    "LSH" : "000D",
+    "INC" : "000E",
+    "DEC" : "000F",
+    "PRR" : "0010"
   }
-
 }
+function lengthen(x){
+  while(x.length < 4){
+    x = "0" + x
+  } return x
+}
+function compile(line){
+  line = line.split()
+  while(line.length < 4){
+    line.push(0)
+  }
+  otp = "0x" + funs[line]
+  otp += lengthen(line[1].toString(16))
+  otp += lengthen(line[2].toString(16))
+  otp += lengthen(line[3].toString(16))
+  return otp
+}
+
+function fullcompile(){
+  otp = "["
+  for (let i = 0; i < rom.length - 1; i++){
+    otp += '"' + compile(rom[i]) + '",'
+  }
+  otp += '"' + compile[rom[rom.length-1]] + '"]"
+  return otp
+}
+
+console.log(fullcompile())
+
+
